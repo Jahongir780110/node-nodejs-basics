6ef2853ca75f268989ee06b6d89b97dcf2ef6465
Jahongir780110 <jahongir780110@gmail.com>
2023-04-04 19:42:01 +0500

    feat: finish cli, modules, hash tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envEntries = Object.entries(process.env);
+  const rsEnvs = envEntries
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`);
+  console.log(rsEnvs.join("; "));
 };
 
 parseEnv();
