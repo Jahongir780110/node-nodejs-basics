3546de89c4c7326399607ef619447cca54409aec
Jahongir780110 <jahongir780110@gmail.com>
2023-04-07 15:22:06 +0500

    feat: finish streams, zip and wt tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import zlib from "zlib";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const pathToCompressedFile = path.join(__dirname, "files", "archive.gz");
+  const pathToDecompressedFile = path.join(__dirname, "files", "test.txt");
+
+  const unzip = zlib.createGunzip();
+
+  const readStream = fs.createReadStream(pathToCompressedFile);
+  const writeStream = fs.createWriteStream(pathToDecompressedFile);
+
+  readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
